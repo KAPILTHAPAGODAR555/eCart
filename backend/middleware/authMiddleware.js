b13b7ffae412037bdef7c920f550ced955869465
Kapil Thapa <kapilthapa1555@gmail.com>
2025-11-06 22:04:09 +0530

    done

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -6,8 +6,9 @@ require('dotenv').config();
 
 module.exports.verfiyToken = (req , res , next) => {
 let authHeader = req.headers['authorization'];
+
 let token = authHeader && authHeader.split(' ')[1];
-// console.log(token);
+
 if(!token)return res.status(401).json({message : "You are not authenticate" , status : false});
 jwt.verify(token , process.env.SECRET_KEY , async(err , data)=> {
     if(err){
